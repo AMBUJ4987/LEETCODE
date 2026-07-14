aeb6e00e11f5745b80b12cd2d37e8e630645febb
Ambuj Tiwari <ambujbtiwari882@gmail.com>
2026-07-14 15:58:39 +0530

    Time: 0 ms (100%), Space: 19.3 MB (65.04%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -2,14 +2,14 @@ from typing import List
 
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        count = [0, 0]
+        c = [0, 0]
 
         for s in students:
-            count[s] += 1
+            c[s] += 1
 
-        for sandwich in sandwiches:
-            if count[sandwich] == 0:
+        for s in sandwiches:
+            if c[s] == 0:
                 break
-            count[sandwich] -= 1
+            c[s] -= 1
 
-        return count[0] + count[1]
+        return c[0] + c[1]
