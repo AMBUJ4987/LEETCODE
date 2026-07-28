1b9dbe0ef270c7cfc9211d1b3e05e3004755fa08
Ambuj Tiwari <ambujbtiwari882@gmail.com>
2026-07-29 01:17:05 +0530

    Time: 2 ms (38.17%), Space: 19.3 MB (24.19%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
@@ -1,10 +1,11 @@
 class Solution:
     def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        res = 0
         x1,y1 = points.pop()
-        a=0
         while points:
-            x2,y2=points.pop()
-            a+= max(abs(x2-x1),abs(y2-y1))
-            x1,y1=x2,y2
-        return a    
-        
+            x2,y2= points.pop()
+            res += max(abs(y2-y1),abs(x2-x1))
+            x1,y1 = x2,y2
+        return res
+
+       
