aae9a73f9f56cf7d39316ff32442a32b7a1c6e6b
Ambuj Tiwari <ambujbtiwari882@gmail.com>
2026-07-29 16:33:22 +0530

    Time: 0 ms (100%), Space: 19.3 MB (67.57%) - LeetHub

---
+++ 0814-binary-tree-pruning/0814-binary-tree-pruning.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        def dfs(node):
+            if not node:
+                return None
+
+            node.left = dfs(node.left)
+            node.right = dfs(node.right)
+
+            if node.val == 0 and node.left is None and node.right is None:
+                return None
+
+            return node
+
+        return dfs(root)
+        
