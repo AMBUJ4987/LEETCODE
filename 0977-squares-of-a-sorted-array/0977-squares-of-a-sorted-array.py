390db4648350eee45b37288ff2aa2fcac53a2812
Ambuj Tiwari <ambujbtiwari882@gmail.com>
2026-07-30 03:36:22 +0530

    Time: 14 ms (28.62%), Space: 21.3 MB (9.78%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,9 +1,17 @@
+from collections import deque 
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        a = []
-        for i in nums:
-            a.append(i**2)
-        b = sorted(a)
-        return b
+        a = deque()
+        l,r = 0, len(nums)-1
+        while l<=r:
+            left,right = abs(nums[l]),abs(nums[r])
+            if left > right:
+                a.appendleft(left*left)
+                l +=1
+            else:
+                a.appendleft(right*right)
+                r-=1
+
+        return list(a)
+
        
-        
